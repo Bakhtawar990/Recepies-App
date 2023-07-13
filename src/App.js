b7e8186aace753947c46fa04aa3a58b8076383dc
Bakhtawar <bakhtawar.rafiq.2332@gmail.com>
2023-07-13 15:33:33 +0500

    ra

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import ImproveSkills from "./components/ImproveSkills";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <div className="container main">
+        <HeroSection/>
+        <ImproveSkills/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
