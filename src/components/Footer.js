2415208ae1a47d88d1c15c55153d1c83624e272f
Bakhtawar <bakhtawar.rafiq.2332@gmail.com>
2023-08-03 12:02:42 +0500

    ra1

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+export default function Footer(){
+    return(
+        <div className="footer container">
+            <div className="footer-section">
+                <p className="title">RecipeApp.com</p>
+                <p>Recipe App is a place where you can please your soul and tummy with delicious food recipes of all cuisine, and our service is absolutely free.</p>
+                <p>&copy; 2023 | All Right Reserved</p>
+            </div>
+            <div className="footer-section">
+                <p className="title">Contact Us</p>
+                <p>recipeapp@gmail.com</p>
+            </div>
+            <div className="footer-section">
+                <p className="title">Socials</p>
+                <p>Facebook</p>
+                <p>Twitter</p>
+                <p>Instagram</p>
+            </div>
+        </div>
+    )
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
+import { Link, useLocation } from "react-router-dom";
 import { useState } from "react";
 import Sidebar from "./Sidebar";
 import { faHome, faList, faCog } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar(){
     const [showSidebar, setShowSidebar] = useState(false)
+    const location = useLocation();
     const links = [
         {
             name: "Home",
@@ -28,14 +30,11 @@ export default function Navbar(){
     return(
         <>
         <div className="navbar container">
-            <a  href="@!" className="logo">R<span>ec</span>ipeApp</a>
+            <Link to="/" className="logo">R<span>ec</span>ipeApp</Link>
             <div className="nav-links">
                 { links.map(link => (
-                    <a href="#!" key={link.name}>{link.name}</a>
+                    <Link className={location.pathname === link.path ? "active" : ""} to= {link.path} key={link.name}>{link.name}</Link>
                 ))}
-                {/* <a href="@!">Home</a>
-                <a href="@!">Recipes</a>
-                <a href="@!">Settings</a> */}
             </div>
             <div onClick={()=>setShowSidebar(true)} className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}>
                 <div className="bar"></div>
